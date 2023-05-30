9af8a99e093e424ab134875b3ad24809459b6569
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-05-30 23:21:44 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -14,9 +14,9 @@ import App from 'App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <BrowserRouter basename="DeliveryApp"> */}
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="DeliveryApp">
+          {/* <BrowserRouter> */}
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
