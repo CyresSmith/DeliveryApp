b3dcb479822bdf3cbdd229ffafab817ab27e4528
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-06-02 13:49:28 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,9 @@ import { setUser } from 'redux/authSlice';
 import { setMediaType } from 'redux/mediaTypeSlice';
 
 import useMediaHook from 'hooks/useMediaHook';
-// import SharedLayout from 'pages/SharedLayout';
-// import Register from 'pages/Register';
-// import Login from 'pages/Login';
-// import History from 'pages/History';
-// import Verify from 'pages/Verify';
-// import Shop from 'pages/Shop';
-// import Cart from 'pages/Cart';
-// import NotFound from 'pages/NotFound';
 
 import PrivateRoute from 'components/PrivateRoute';
+import Spinner from 'components/Shared/Spinner';
 
 const SharedLayout = lazy(() => import('pages/SharedLayout'));
 const Register = lazy(() => import('pages/Register'));
@@ -51,26 +44,26 @@ function App() {
 
   return (
     <>
-      {/* <Suspense fallback={<Spinner />}> */}
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Shop />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/history"
-            element={
-              <PrivateRoute>
-                <History />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/verify/:token" element={<Verify />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} /> */
-        </Route>
-      </Routes>
-      {/* </Suspense> */}
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Shop />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/history"
+              element={
+                <PrivateRoute>
+                  <History />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/verify/:token" element={<Verify />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} /> */
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
